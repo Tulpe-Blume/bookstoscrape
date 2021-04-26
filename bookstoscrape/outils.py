3f3fbf2cde355d75b9ecafd4a63dd8ef87377b04
Tulpe-Blume <clement-aserti@gmx.fr>
2021-04-26 16:54:48 +0200

    Fonction get_page modifiée le httperror fonctionne

--- bookstoscrape/outils.py
+++ bookstoscrape/outils.py
@@ -10,47 +10,37 @@ def get_page(url):
     et de tester s'il n'y a pas de problème"""
     try:
         response = rq.get(url)
+        response.raise_for_status()
     except rq.exceptions.ConnectionError:
         return ("Il y a un problème réseau vérifier si votre réseau fonctionne.")
     except rq.exceptions.Timeout:
-        return ("Votre demande a expirée")
-    except rq.exceptions.HTTPError:
-        return ("La page n'est pas joingnable")
+        return ("Votre demande a expirée.")
+    except rq.exceptions.HTTPError: 
+        return ("La page n'est pas joignable")
     except rq.exceptions.TooManyRedirects:
         return ("Le nombre de redirections est dépassé")
-    finally:
-        pass 
-    return response
+    else:
+        return ("La page est joignable et peut être analysée!")
 
-def create_soup(in_response):
+
+page = rq.get(url)
+
+
+def create_soup(url, page):
     """Fonction permettant de créer la soupe"""
-    page_soup = bs(in_response.content.decode("utf-8", "ignore"), "html.parser")
-    return page_soup
+    page = rq.get(url)
+    if page.raise_for_status() is page.ok:
+        page_soup = bs(page.content.decode("utf-8", "ignore"), "html.parser")
+        return (page_soup.p.text)
+    else:
+        pass #leave the programme.
+
+print(get_page(url))
+print(create_soup(url, page))
 
 
-print("CHECKPOINT -- outils.py")
-# Récuperer le contenu de la page
-reponse = get_page(url)
 # Créer la classe qui analyse le contenu de la page
 #page_soupe = bs(response.content)
 
 #print(page_soupe)
-#print(type(page_soupe))
-print("CHECKPOINT -- outils.py")
-
-
-#resp = get_page(url)
-#make_soup(resp)
-#print(resp)
-#print(resp_json)
-
-
-#def product_description():
- #   """Fonction recuperant le contenu de la description produit si elle existe"""
-  #  if  existe:
-   #     pass
-#page_soup = bs(reponse.content)
-
-
-#print(page.status_code)
-#print(page.headers)
+#print(type(page_soupe)
