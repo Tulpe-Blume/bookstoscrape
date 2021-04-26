9ec3d6462271ac3985b5323d070b8156aad78b93
Tulpe-Blume <clement-aserti@gmx.fr>
2021-04-26 21:29:03 +0200

    Fonction dans une get_page

--- bookstoscrape/outils.py
+++ bookstoscrape/outils.py
@@ -8,38 +8,38 @@ url = 'http://books.toscrape.com/catalogue/dune-dune-1_151/index.html'
 
 def get_page(url):
     """Fonction permettant de  vérifier si la page existe 
-    et de tester s'il n'y a pas de problème"""
+    et de tester s'il n'y a pas de problème
+    et de retourner la soupe associée"""
     try:
         response = rq.get(url)
         response.raise_for_status()
     except rq.exceptions.ConnectionError:
-        return ("Il y a un problème réseau vérifier si votre réseau fonctionne.")
+        print("Il y a un problème réseau vérifier si votre réseau fonctionne.")
+        sys.exit()  # Quitte le programme
     except rq.exceptions.Timeout:
-        return ("Votre demande a expirée.")
-    except rq.exceptions.HTTPError: 
-        return ("La page n'est pas joignable")
+        print("Votre demande a expirée.")
+        sys.exit()  # Quitte le programme
+    except rq.exceptions.HTTPError:
+        print("La page n'est pas joignable")
+        sys.exit()  # Quitte le programme
     except rq.exceptions.TooManyRedirects:
-        return ("Le nombre de redirections est dépassé")
+        print("Le nombre de redirections est dépassé")
+        sys.exit()  # Quitte le programme
     else:
-        return ("La page est joignable et peut être analysée!")
+        page_soup = bs(response.content.decode("utf-8", "ignore"), "html.parser")
+        return (page_soup)
+        
 
+#def create_soup(url):
+#    """Fonction permettant de créer la soupe si la page existe"""
+    # # page = rq.get(url)
+    # page = get_page(url)
+    # if page.ok:
+    #     page_soup = bs(page.content.decode("utf-8", "ignore"), "html.parser")
+    #     return (page_soup.prettify)
+    # else:
+    #     sys.exit()  # Quitte le programme.
 
-def create_soup(url):
-    """Fonction permettant de créer la soupe si la page existe"""
-    page = rq.get(url)
-    if page.ok:
-        page_soup = bs(page.content.decode("utf-8", "ignore"), "html.parser")
-        return (page_soup.prettify)
-    else:
-        sys.exit()  # Quitte le programme.
-
-
-print(get_page(url))
-print(create_soup(url))
-
-
-# Créer la classe qui analyse le contenu de la page
-#page_soupe = bs(response.content)
-
-#print(page_soupe)
-#print(type(page_soupe)
+if __name__ == "__main__":
+    print(get_page(url).prettify())
+    # print(create_soup(url))
