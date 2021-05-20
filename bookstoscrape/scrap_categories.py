844c768e00706a98e9e6fb06747d54c9887a4ead
Tulpe-Blume <clement-aserti@gmx.fr>
2021-05-20 09:48:52 +0200

    rajout de fonctions pour save_csv + get_all_books_page url finie

--- bookstoscrape/scrap_categories.py
+++ bookstoscrape/scrap_categories.py
@@ -1,18 +1,22 @@
 from pprint import pprint
 import os
 from urllib.parse import urljoin
+import csv
 
-import requests
+import requests as rq
 from bs4 import BeautifulSoup as BSoup
+from slugify import slugify
 
 from outils import get_page
 from scrap_books import get_book_info
+from scrap_books import main as scrap_book_main
+from scrap_all import get_categories_url
 
 
-def get_book_page_url(soup, url):
+def get_book_page_url_list(soup, url):
     """Fonction permettant de récupérer tous les liens 
     d'une categorie"""
-    book_link_list = []
+    book_urls_list = []
 
     section = soup.find("div", class_="col-sm-8 col-md-9").find("section")
     book_group = section.find_all("div")[1].find("ol", class_="row")
@@ -23,40 +27,53 @@ def get_book_page_url(soup, url):
         # le lien est relatif ... passage au lien absolu obligatoire
         link = urljoin(url, relative_url)
         # on ajoute le lien courant dans la liste de liens
-        book_link_list.append(link)
-    return book_link_list
+        book_urls_list.append(link)
+    return book_urls_list
 
 
-# def is_next_page(soup):
-#     """Fonction indiquant s'il existe une page suivante"""
-#     next_page = 
-#     return
+def is_next_page(soup, url):
+    """Fonction qui indique le lien vers une page suivante"""
+    try:
+        relative_next_page = soup.select_one(".next > a").get("href")
+        # Le lien est relatif passage au lien absolu
+        next_page = urljoin(url, relative_next_page)
+        return next_page
+    except AttributeError:
+        return None
 
 
-# def get_next_page(soup):
-#     """Fonction permettant d'aller à la page suivante"""
-#     return next_url
+def get_all_books_page_url(soup, url):
+    """Fonction permettant de récupérer les liens 
+    des pages supplémentaires et de les rajouter à
+    la liste des liens"""
+    # on recupere la liste de la page courante
+    book_list = get_book_page_url_list(soup, url)
+    # pprint(book_list)
 
+    curr_url = url
 
-# def get_all_books_page_url(soup, url):
-#     # on recupere la liste de la page courante
-#     book_list = get_book_page_url(soup, url)
+    # tant qu'il existe un lien suivant...
+    while is_next_page(soup, url=curr_url):
+        # on suit le (lapin blanc) lien suivant
+        next_url = is_next_page(soup, url=curr_url)
+        # et on lui crée une soupe
+        soup = get_page(next_url)
+        # print("CURR PAGE: " + curr_url + "   ->    NEXT PAGE: " + next_url)
 
-#     # tant qu'il existe un lien suivant...
-#     while is_next_page():
+        # on récupère la liste de cette page ...
+        curr_book_list = get_book_page_url_list(soup, url=next_url)
+        # print("****")
+        # print("La liste issue de la page " + next_url)
+        # pprint(curr_book_list)
+        # print("****")
 
-#         # on suit le (lapin blanc) lien suivant
-#         next_url = get_next_page(soup)
-#         # et on lui crée une soupe
-#         next_soup = get_page(next_url)
-#         # on récupère la liste de cette page ...
-#         curr_book_list = get_book_page_url(next_soup, next_url)
-#         # on ajoute la liste courrante à la liste précédente
-#         for curr_book in curr_book_list:
-#             book_list.append(curr_book)
-
-#     # on retourne la liste complete
-#     return
+        # on ajoute la liste courrante à la liste précédente
+        for curr_book in curr_book_list:
+            book_list.append(curr_book)
+        # pprint(book_list)
+        curr_url = next_url
+    # on retourne la liste complete
+    return book_list
 
 
 def create_csv_directory():
@@ -67,26 +84,88 @@ def create_csv_directory():
         return ("Repertoire csv cree")
     except FileExistsError:
         return ("Le repertoire existe deja.")
-
-
-def save_book_into_csv(csvfile, book_info):
-    """Fonction permettant d'enregistrer les informations d'un
-     livre dans un fichier csv"""
-    pass
-    
     
 
+# def create_csv_file():
+#     """Fonction permettant de créer le fichier csv"""
+#     # Mettre les différentes catégories dans une liste
+#     #categories_list = 
+#     #try:
+#      #   os.mkdir()
+#     pass
 
-def main():
-    """Doc"""
-    print("Dans scrap_categories")
+def save_books_into_csv(csv_filename, book_info_list):
+    """Fonction permettant d'enregistrer les informations des
+       livres d'une catégorie dans un fichier csv"""
+    with open(csv_filename, 'w', newline='') as csvfile:
+        header = ['product_page_url', 'universal_product_code', 'title', 'price_including_tax',
+                  'price_excluding_tax', 'number_available', 'product_description',
+                  'category', 'review_rating', 'image_url', 'new_img_name']
+        writer = csv.DictWriter(csvfile, fieldnames=header)
+        writer.writeheader()
+        for elt_dict in book_info_list:
+            writer.writerow(elt_dict)
+    return "The books information is saved into the CSV file"
 
-    url = 'http://books.toscrape.com/catalogue/category/books/mystery_3/index.html'
-    soup = get_page(url)
-    pprint(get_book_page_url(soup, url))
 
+def get_category():
+    return "la category"
+
+def main(url, soup):
+    """Fonction qui scrape les livres d'une catégorie.
+    Et qui retourne une liste de dictionnaires"""
+
+    # Créer une liste vide
+    all_book_info_list = []
+
+    # Récupération de la liste des urls de chaque livre
+    book_url_list = get_all_books_page_url(soup, url)
+    
+    for book_url in book_url_list:
+        # pour chaque livre, on récupère les infos (sous forme de dict)
+        book_info_dict = scrap_book_main(url=book_url)
+        # TODO ajouter la categorie comme entrée dans chaque dictionnaire
+        # on ajoute la categorie
+        book_info_dict['category'] = get_category()
+        # on ajoute le dict récupéré à la liste
+        all_book_info_list.append(book_info_dict)
+
+    # on stocke toutes ces infos dans un CSV
     # Attention création du répertoire pour les csv
     print(create_csv_directory())
 
+    # nommer le fichier CSV en utilisant la categorie
+    csv_filename = slugify(get_category(), max_length=80) + ".csv"
+
+    # remplir le csv avec les infos de tous les livres
+    save_books_into_csv(csv_filename, book_info_list=all_book_info_list)
+
+    return all_book_info_list
+
+
 if __name__ == "__main__":
-    main()
+    print("Dans scrap_categories")
+    url = 'http://books.toscrape.com/catalogue/category/books/mystery_3/index.html'
+    soup = get_page(url)
+
+    print("URL de la catégorie")
+    print(url)
+
+    print("\n\ntest de get_book_page_url_list")    
+    pprint(get_book_page_url_list(soup, url=url))
+    
+    print("\n\ntest de is_next_page")
+    print(is_next_page(soup, url=url))
+
+    print("\n\ntest de get_all_books_page_url")
+    pprint(get_all_books_page_url(soup, url=url))
+
+    print("\n\ntest de main")
+    pprint(main(url, soup))
+
+
+
+#     soup = get_page('http://books.toscrape.com/catalogue/dune-dune-1_151/index.html')
+#     book_info = get_book_info(soup)
+#     #with open('a_category.csv') as csvfile:
+#     #    save_book_into_csv(csvfile, book_info)
