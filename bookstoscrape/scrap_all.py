a91949acb4e67e6bb6467b5798f9b9783311e9f7
Tulpe-Blume <clement-aserti@gmx.fr>
2021-05-20 09:50:01 +0200

    rajout dans fonction main

--- bookstoscrape/scrap_all.py
+++ bookstoscrape/scrap_all.py
@@ -1,4 +1,5 @@
 from pprint import pprint
+from urllib.parse import urljoin
 
 from bs4 import BeautifulSoup as BSoup
 
@@ -8,15 +9,24 @@ from scrap_books import get_book_info
 def get_categories_url(soup, url):
     """Fonction permettant de récupérer tous 
     les liens des catégories"""
-    categories_links_list = []
-    categories_link
-    pass
-
+    categories_urls_list = []
+    categories_group = soup.select_one(".nav-list  ul")
+    
+    for categories_block in categories_group.find_all("li"):
+        relative_url = categories_block.find("a").get("href")
+        # Le lien est relatif ... passage au lien absolu obligatoire
+        link = urljoin(url, relative_url)
+        # On ajoute le lien courant dans la liste de liens
+        categories_urls_list.append(link)
+    
+    return categories_urls_list
 
 
 def main():
     print("dans scrap_all.py")
-
+    url = "http://books.toscrape.com/index.html"
+    soup = get_page(url)
+    pprint(get_categories_url(soup, url))
 
 if __name__ == "__main__":
     main()
