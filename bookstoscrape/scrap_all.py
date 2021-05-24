da0fad9631e65df5676fd5a623e717eadae2082a
Tulpe-Blume <clement-aserti@gmx.fr>
2021-05-24 17:58:36 +0200

    Nettoyage imports, liaison du main() de scrap_categories, boucle sur url, code fini

--- bookstoscrape/scrap_all.py
+++ bookstoscrape/scrap_all.py
@@ -1,10 +1,9 @@
 from pprint import pprint
 from urllib.parse import urljoin
 
-from bs4 import BeautifulSoup as BSoup
-
 from outils import get_page
-from scrap_books import get_book_info
+from scrap_categories import main as scrap_categories_main
+
 
 def get_categories_url(soup, url):
     """Fonction permettant de récupérer tous 
@@ -26,8 +25,15 @@ def main():
     print("dans scrap_all.py")
     url = "http://books.toscrape.com/index.html"
     soup = get_page(url)
-    pprint(get_categories_url(soup, url))
+
+    # on récupère la liste des urls de toutes les categories
+    all_categories_url_list = get_categories_url(soup, url)
+
+    # pour chacune, on appelle scrap_categories (qui va faire tout...)
+    for category_url in all_categories_url_list:
+        scrap_categories_main(category_url)
+
 
 if __name__ == "__main__":
-    main()
+    print(main())
 
